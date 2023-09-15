ed0c31e1e03a0353442a2c05ac57eec8473a0e7d
Montoya24 <145078948+Montoya24@users.noreply.github.com>
2023-09-15 07:34:46 +0000

    🎈cambios

---
+++ server/app.js
@@ -0,0 +1,46 @@
+var debug = require('debug')('project_pilaII:server');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//  Creando la instancia de express
+var app = express();
+
+// Configurando el motor de plantillas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+//  Se establecen los Middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+//  Crea un server de archivos estaticos
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/', indexRouter);
+//  Activa "usersRouter" cuando se solicita
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,1 +1,13 @@
+var express = require('express');
+var router = express.Router();
 
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/author', function(_, res) {
+  res.render('author', { author: "Espinoza Montoya" })
+});
+
+module.exports = router;
